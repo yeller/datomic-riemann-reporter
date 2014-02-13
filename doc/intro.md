7b054789aabad2e558827ca8ddf0914ea09507d1
Tom Crayford <tcrayford@googlemail.com>
2014-02-13 20:30:59 +0000

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datomic-riemann-reporter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/datomic_riemann_reporter/core_test.clj
@@ -0,0 +1,7 @@
+(ns datomic-riemann-reporter.core-test
+  (:require [clojure.test :refer :all]
+            [datomic-riemann-reporter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
