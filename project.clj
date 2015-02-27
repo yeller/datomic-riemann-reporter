feb6c27f560091eaa9f786af62fb60d7ba89c693
Martin Harrigan <martinharrigan@gmail.com>
2015-02-27 16:41:57 +0000

    Update riemann-clojure-client and send-event function name

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [riemann-clojure-client "0.2.9"]
+                 [riemann-clojure-client "0.3.2"]
                  [environ "0.4.0"]])
