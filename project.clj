a3970275a9349607b4080413ecc04caa38c3ecba
Tom Crayford <tcrayford@googlemail.com>
2015-02-28 15:39:40 +0800

    release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datomic-riemann-reporter "0.1.0-SNAPSHOT"
+(defproject datomic-riemann-reporter "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
